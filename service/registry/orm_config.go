440a5c5f45c781681ecf1e040ce7902d3785fe71
Anton <a.shumansky@gmail.com>
2021-03-08 18:08:38 +0200

    implement google oss

--- service/registry/orm_config.go
+++ service/registry/orm_config.go
@@ -3,7 +3,6 @@ package registry
 import (
 	"github.com/coretrix/hitrix/pkg/entity"
 	"github.com/coretrix/hitrix/service"
-	"github.com/coretrix/hitrix/service/component/oss"
 	"github.com/sarulabs/di"
 
 	"github.com/summer-solutions/orm"
@@ -19,8 +18,8 @@ func ServiceDefinitionOrmRegistry(init ORMRegistryInitFunc) *service.Definition 
 			registry := orm.InitByYaml(service.DI().Config().Get("orm").(map[string]interface{}))
 			init(registry)
 
-			ossService := ctn.Get("oss_google").(oss.Client)
-			if ossService != nil {
+			_, err := ctn.SafeGet("oss_google")
+			if err != nil {
 				registry.RegisterEntity(&entity.OSSBucketCounterEntity{})
 			}
 
