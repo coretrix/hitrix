e4963a9445584efed6e3285488a6752093338cc8
Mohammad <mnazarin92@gmail.com>
2021-03-10 17:28:23 +0100

    added jwt verify

--- test/jwt_test.go
+++ test/jwt_test.go
@@ -76,3 +76,25 @@ func TestVerifyJWTExpired(t *testing.T) {
 	err := jwt.VerifyJWT("mynewsecret", jwtToken, 72000)
 	assert.NoError(t, err, "Cannot verify expired jwt token")
 }
+
+func TestExtractPayload(t *testing.T) {
+	expectedTokenSubject := "User"
+	headers := map[string]string{
+		"algo": "HS256",
+		"type": "JWT",
+	}
+
+	payload := map[string]string{
+		"exp": fmt.Sprintf("%v", time.Now().Unix()),
+		"iss": "bluelog",
+		"sub": expectedTokenSubject,
+	}
+
+	jwt := &jwt2.JWT{}
+
+	jwtToken, _ := jwt.EncodeJWT("mynewsecret", headers, payload)
+
+	extractedPayload, err := jwt.VerifyJWTAndGetPayload("mynewsecret", jwtToken, 72000)
+	assert.NoError(t, err, "Cannot verify expired jwt token")
+	assert.Equal(t, expectedTokenSubject, extractedPayload["sub"], "Cannot create jwt token!")
+}
