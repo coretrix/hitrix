e428228304360e0c95306f72ca853d681d8333cf
Mohammad <mnazarin92@gmail.com>
2021-04-07 14:02:47 +0200

    better

--- test/api_logger_test.go
+++ test/api_logger_test.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-
-	"github.com/99designs/gqlgen/graphql"
 	"github.com/coretrix/hitrix/example/entity"
-	"github.com/coretrix/hitrix/pkg/test"
 	"github.com/coretrix/hitrix/service"
 	"github.com/coretrix/hitrix/service/registry"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func TestCreateContext(t *testing.T) {
+func TestApiLogger(t *testing.T) {
 	createContextMyApp(t, "my-app", nil, []*service.Definition{registry.APILogger(&entity.APILogEntity{})})
 
 	apiLoggerService, has := service.DI().APILoggerService()
@@ -33,20 +29,4 @@ func TestCreateContext(t *testing.T) {
 	assert.Len(t, apiLogEntities, 2)
 	assert.Equal(t, apiLogEntities[0].Status, entity.APILogStatusCompleted)
 	assert.Equal(t, apiLogEntities[1].Status, entity.APILogStatusFailed)
-}
-
-func createContextMyApp(t *testing.T, projectName string, resolvers graphql.ExecutableSchema, additionalServices []*service.Definition) *test.Environment {
-	defaultServices := []*service.Definition{
-		registry.ServiceProviderConfigDirectory("../example/config"),
-		registry.ServiceDefinitionOrmRegistry(entity.Init),
-		registry.ServiceDefinitionOrmEngine(),
-		registry.ServiceDefinitionOrmEngineForContext(),
-	}
-
-	return test.CreateContext(t,
-		projectName,
-		resolvers,
-		nil,
-		append(defaultServices, additionalServices...),
-	)
 }
---
+++ test/test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/coretrix/hitrix/example/entity"
+	"github.com/coretrix/hitrix/pkg/test"
+	"github.com/coretrix/hitrix/service"
+	"github.com/coretrix/hitrix/service/registry"
+)
+
+func createContextMyApp(t *testing.T, projectName string, resolvers graphql.ExecutableSchema, additionalServices []*service.Definition) *test.Environment {
+	defaultServices := []*service.Definition{
+		registry.ServiceProviderConfigDirectory("../example/config"),
+		registry.ServiceDefinitionOrmRegistry(entity.Init),
+		registry.ServiceDefinitionOrmEngine(),
+		registry.ServiceDefinitionOrmEngineForContext(),
+	}
+
+	return test.CreateContext(t,
+		projectName,
+		resolvers,
+		nil,
+		append(defaultServices, additionalServices...),
+	)
+}
