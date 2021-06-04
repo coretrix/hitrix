8f006a258aceee599f7929aff377a01f81e07843
Anton <a.shumansky@gmail.com>
2021-06-04 21:44:13 +0300

    add fake mail registry

---
+++ service/registry/mocks/mail.go
@@ -0,0 +1,18 @@
+package mocks
+
+import (
+	"github.com/coretrix/hitrix/service"
+	"github.com/coretrix/hitrix/service/component/mail"
+
+	"github.com/sarulabs/di"
+)
+
+func FakeMailService(fakeMailSender mail.Sender) *service.Definition {
+	return &service.Definition{
+		Name:   service.MailMandrill,
+		Global: true,
+		Build: func(ctn di.Container) (interface{}, error) {
+			return fakeMailSender, nil
+		},
+	}
+}
