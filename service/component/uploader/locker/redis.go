29ee10f8469feae9911974a9db1d7de98e619d11
Iliyan <iliyan.motovski@coretrix.com>
2021-07-05 15:23:44 +0300

    small fix

--- service/component/uploader/locker/redis.go
+++ service/component/uploader/locker/redis.go
@@ -10,23 +10,21 @@ import (
 )
 
 type RedisLocker struct {
-	ctx        context.Context
 	ormService *orm.Engine
 }
 
 func (locker *RedisLocker) NewLock(id string) (tusd.Lock, error) {
-	return &redisLock{id: id, ctx: locker.ctx, redis: locker.ormService.GetRedis()}, nil
+	return &redisLock{id: id, redis: locker.ormService.GetRedis()}, nil
 }
 
 type redisLock struct {
 	id        string
-	ctx       context.Context
 	redis     *orm.RedisCache
 	redisLock *orm.Lock
 }
 
 func (lock *redisLock) Lock() error {
-	redisLock, obtained := lock.redis.GetLocker().Obtain(lock.ctx, "tusd:upload:lock:"+lock.id, time.Hour*24, time.Second*2)
+	redisLock, obtained := lock.redis.GetLocker().Obtain(context.Background(), "tusd:upload:lock:"+lock.id, time.Hour*24, time.Second*2)
 	if !obtained {
 		return errors.New("cannot obtain lock")
 	}
