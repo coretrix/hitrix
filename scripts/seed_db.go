68f04d8affa19d6786da20738b5b092ad1dff38d
alhasaniq <alhasan.nasiry@gmail.com>
2021-09-03 16:55:16 +0300

    Adding Settings + Seeds

---
+++ scripts/seed_db.go
@@ -0,0 +1,90 @@
+package scripts
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/coretrix/hitrix"
+	"github.com/latolukasz/beeorm"
+
+	"github.com/coretrix/hitrix/pkg/entity"
+	"github.com/coretrix/hitrix/service"
+)
+
+type DBSeedScript struct {
+	Seeds map[string]Seed
+}
+
+func (script *DBSeedScript) Run(ctx context.Context, _ hitrix.Exit) {
+	ormService, _ := service.DI().OrmEngine()
+	Seeder(script.Seeds, ormService)
+}
+
+func (script *DBSeedScript) Unique() bool {
+	return true
+}
+
+func (script *DBSeedScript) Description() string {
+	return "Seed Database"
+}
+
+type Seed interface {
+	Execute(*beeorm.Engine)
+	Version() int
+}
+
+func Seeder(seeds map[string]Seed, ormService *beeorm.Engine) {
+
+	var setting entity.SettingsEntity
+
+	whereStmt := beeorm.NewWhere("`Key` = ?", entity.HitrixSettingAll.Seeds)
+	var hasExecutedSeedsSetting = ormService.SearchOne(whereStmt, &setting)
+
+	var executedSeeds entity.SettingSeedsValue
+	if hasExecutedSeedsSetting {
+		if err := json.Unmarshal([]byte(setting.Value), &executedSeeds); err != nil {
+			panic(err.Error())
+		}
+	}
+
+	var newSeeds entity.SettingSeedsValue = make(entity.SettingSeedsValue)
+
+	for k, seed := range seeds {
+		_, hasExecutedSeed := executedSeeds[k]
+		if !hasExecutedSeedsSetting || !hasExecutedSeed ||
+			(hasExecutedSeed && executedSeeds[k] < seed.Version()) {
+			seed.Execute(ormService)
+			newSeeds[k] = seed.Version()
+		}
+	}
+	if len(newSeeds) > 0 {
+		saveNewSeeds(ormService, newSeeds)
+	}
+
+}
+func saveNewSeeds(ormService *beeorm.Engine, newSeeds entity.SettingSeedsValue) {
+	var settings = entity.SettingsEntity{
+		Key: entity.HitrixSettingAll.Seeds,
+	}
+
+	var hasExecutedSeedsSetting = ormService.Load(&settings)
+
+	if hasExecutedSeedsSetting {
+		var oldSeeds entity.SettingSeedsValue
+		if err := json.Unmarshal([]byte(settings.Value), &oldSeeds); err != nil {
+			panic(err.Error())
+		}
+
+		// overwrite old with newSeeds
+		for k, v := range newSeeds {
+			oldSeeds[k] = v
+		}
+	}
+
+	str, err := json.Marshal(newSeeds)
+	if err != nil {
+		panic(err.Error())
+	}
+	settings.Value = string(str)
+	ormService.Flush(&settings)
+}
