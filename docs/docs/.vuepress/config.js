e4700cfc30029f5169449c53e56df8d82b3eafe3
Anton <a.shumansky@gmail.com>
2021-10-15 14:41:55 +0300

    fix docs

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
     title: 'Hitrix - golang framework',
     description: 'Golang Framework for high traffic applications. Designed for speed up development time.',
     head: [
