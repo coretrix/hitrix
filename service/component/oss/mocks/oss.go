16686708e5bdbdaa5fbe1230500445a2ce3f537f
Krasimir Ivanov <krasimir.ivanov@coretrix.com>
2022-02-23 23:56:17 +0200

    mocks

--- service/component/oss/mocks/oss.go
+++ service/component/oss/mocks/oss.go
@@ -17,54 +17,54 @@ func (t *FakeOSSClient) GetClient() interface{} {
 	return nil
 }
 
-func (t *FakeOSSClient) GetObjectURL(bucket string, object *oss.Object) (string, error) {
+func (t *FakeOSSClient) GetObjectURL(bucket oss.Bucket, object *oss.Object) (string, error) {
 	return t.Called(bucket, object).Get(0).(string), nil
 }
 
-func (t *FakeOSSClient) GetObjectOSSURL(bucket string, object *oss.Object) (string, error) {
+func (t *FakeOSSClient) GetObjectOSSURL(bucket oss.Bucket, object *oss.Object) (string, error) {
 	return t.Called(bucket, object).Get(0).(string), nil
 }
 
-func (t *FakeOSSClient) GetObjectCDNURL(bucket string, object *oss.Object) (string, error) {
+func (t *FakeOSSClient) GetObjectCDNURL(bucket oss.Bucket, object *oss.Object) (string, error) {
 	return t.Called(bucket, object).Get(0).(string), nil
 }
 
-func (t *FakeOSSClient) GetObjectSignedURL(bucket string, object *oss.Object, expires time.Time) (string, error) {
+func (t *FakeOSSClient) GetObjectSignedURL(bucket oss.Bucket, object *oss.Object, expires time.Time) (string, error) {
 	return t.Called(bucket, object, expires).Get(0).(string), nil
 }
 
-func (t *FakeOSSClient) GetObjectBase64Content(bucket string, object *oss.Object) (string, error) {
+func (t *FakeOSSClient) GetObjectBase64Content(bucket oss.Bucket, object *oss.Object) (string, error) {
 	return t.Called(bucket, object).Get(0).(string), nil
 }
 
-func (t *FakeOSSClient) UploadObjectFromFile(_ *beeorm.Engine, bucket, path, localFile string) (oss.Object, error) {
+func (t *FakeOSSClient) UploadObjectFromFile(_ *beeorm.Engine, bucket oss.Bucket, path oss.Namespace, localFile string) (oss.Object, error) {
 	return t.Called(bucket, path, localFile).Get(0).(oss.Object), nil
 }
 
-func (t *FakeOSSClient) UploadObjectFromBase64(_ *beeorm.Engine, bucket, path, content, extension string) (oss.Object, error) {
+func (t *FakeOSSClient) UploadObjectFromBase64(_ *beeorm.Engine, bucket oss.Bucket, path oss.Namespace, content, extension string) (oss.Object, error) {
 	return t.Called(bucket, path, content, extension).Get(0).(oss.Object), nil
 }
 
-func (t *FakeOSSClient) UploadObjectFromByte(_ *beeorm.Engine, bucket, path string, content []byte, extension string) (oss.Object, error) {
+func (t *FakeOSSClient) UploadObjectFromByte(_ *beeorm.Engine, bucket oss.Bucket, path oss.Namespace, content []byte, extension string) (oss.Object, error) {
 	return t.Called(bucket, path, content, extension).Get(0).(oss.Object), nil
 }
 
-func (t *FakeOSSClient) UploadImageFromFile(_ *beeorm.Engine, bucket, path, localFile string) (oss.Object, error) {
+func (t *FakeOSSClient) UploadImageFromFile(_ *beeorm.Engine, bucket oss.Bucket, path oss.Namespace, localFile string) (oss.Object, error) {
 	return t.Called(bucket, path, localFile).Get(0).(oss.Object), nil
 }
 
-func (t *FakeOSSClient) UploadImageFromBase64(_ *beeorm.Engine, bucket, path, image, extension string) (oss.Object, error) {
+func (t *FakeOSSClient) UploadImageFromBase64(_ *beeorm.Engine, bucket oss.Bucket, path oss.Namespace, image, extension string) (oss.Object, error) {
 	return t.Called(bucket, path, image, extension).Get(0).(oss.Object), nil
 }
 
-func (t *FakeOSSClient) DeleteObject(_ string, _ *oss.Object) error {
+func (t *FakeOSSClient) DeleteObject(_ oss.Bucket, _ *oss.Object) error {
 	return nil
 }
 
-func (t *FakeOSSClient) CreateObjectFromKey(_ *beeorm.Engine, _, _ string) oss.Object {
+func (t *FakeOSSClient) CreateObjectFromKey(_ *beeorm.Engine, _ oss.Bucket, _ string) oss.Object {
 	return oss.Object{}
 }
 
-func (t *FakeOSSClient) GetUploaderBucketConfig() *oss.BucketConfig {
-	return &oss.BucketConfig{}
+func (t *FakeOSSClient) GetBucketConfig(_ oss.Bucket) *oss.BucketConfig {
+	return nil
 }
