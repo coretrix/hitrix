d825b601ecb828a329aac698baacfd635734f1a4
mahya.ghadiri <mahya.ghadiri@snapp.cab>
2022-02-23 22:38:18 +0330

    create invoice added

--- service/component/elorus/elorus.go
+++ service/component/elorus/elorus.go
@@ -30,6 +30,26 @@ type CreateContactRequest struct {
 	} `json:"phones"`
 }
 
+type CreateInvoiceRequest struct {
+	Date              string `json:"date"`
+	Client            string `json:"client"`
+	ClientDisplayName string `json:"client_display_name"`
+	ClientVatNumber   string `json:"client_vat_number"`
+	ClientEmail       string `json:"client_email"`
+	Items             []struct {
+		Product                      string   `json:"product"`
+		Title                        string   `json:"title"`
+		Description                  string   `json:"description"`
+		Quantity                     string   `json:"quantity"`
+		UnitMeasure                  int      `json:"unit_measure"`
+		UnitValue                    string   `json:"unit_value"`
+		Taxes                        []string `json:"Taxes"`
+		UnitTotal                    string   `json:"unit_total"`
+		MydataClassificationCategory string   `json:"mydata_classification_category"`
+		MydataClassificationType     string   `json:"mydata_classification_type"`
+	} `json:"items"`
+}
+
 type ElorusResponse struct {
 	ID string `json:"id"`
 }
@@ -49,6 +69,35 @@ func (e *Elorus) CreateContact(request *CreateContactRequest) (*ElorusResponse, 
 
 	jsonReq, _ := json.Marshal(request)
 	req, err := http.NewRequest("POST", e.url+"/contacts/", bytes.NewBuffer(jsonReq))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Token "+e.token)
+	req.Header.Set("X-Elorus-Organization", e.organizationID)
+	if e.environment != "prod" {
+		req.Header.Set("X-Elorus-Demo", "true")
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(ElorusResponse)
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (e *Elorus) CreateInvoice(request *CreateInvoiceRequest) (*ElorusResponse, error) {
+	client := &http.Client{}
+
+	jsonReq, _ := json.Marshal(request)
+	req, err := http.NewRequest("POST", e.url+"/invoices/", bytes.NewBuffer(jsonReq))
 	if err != nil {
 		return nil, err
 	}
--- service/component/elorus/elorus_provider.go
+++ service/component/elorus/elorus_provider.go
@@ -2,4 +2,5 @@ package elorus
 
 type IProvider interface {
 	CreateContact(request *CreateContactRequest) (*ElorusResponse, error)
+	CreateInvoice(request *CreateInvoiceRequest) (*ElorusResponse, error)
 }
