02114bbd4e83e783f937d5214496718483dd34d8
Krasimir Ivanov <krasimir.ivanov@coretrix.com>
2022-03-06 17:01:57 +0200

    cleaning

--- example/rest/middleware/router.go
+++ example/rest/middleware/router.go
@@ -1,22 +1,12 @@
 package middleware
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/coretrix/hitrix/example/rest/controller"
-	"github.com/coretrix/hitrix/service"
-	"github.com/coretrix/hitrix/service/component/oss"
 )
 
 func Router(ginEngine *gin.Engine) {
 	var websocketController *controller.WebsocketController
 	ginEngine.GET("/ws/", websocketController.InitConnection)
-
-	ginEngine.GET("/dev/upload/", func(context *gin.Context) {
-		object := service.DI().OSService().GetBucketConfig(oss.BucketPublic)
-
-		log.Fatal(object)
-	})
 }
