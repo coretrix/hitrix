e4340d1018e314c3accebb3f7fab765cdbc0af8b
Iliyan <iliyan.motovski@coretrix.com>
2022-04-08 17:19:55 +0300

    optimize reindex all redisearch

--- pkg/queue/consumer.go
+++ pkg/queue/consumer.go
@@ -135,8 +135,8 @@ func (r *ConsumerRunner) RunConsumerOneByModulo(consumer ConsumerOneByModulo, gr
 	outerWG.Add(maxModulo)
 
 	for moduloID := 1; moduloID <= maxModulo; moduloID++ {
+		currentModulo := moduloID
 		hitrix.GoroutineWithRestart(func() {
-			currentModulo := moduloID
 
 			queueName := consumer.GetQueueName(currentModulo)
 			consumerGroupName := consumer.GetGroupName(currentModulo, groupNameSuffix)
@@ -197,9 +197,8 @@ func (r *ConsumerRunner) RunConsumerManyByModulo(consumer ConsumerManyByModulo, 
 	outerWG.Add(maxModulo)
 
 	for moduloID := 1; moduloID <= maxModulo; moduloID++ {
+		currentModulo := moduloID
 		hitrix.GoroutineWithRestart(func() {
-			currentModulo := moduloID
-
 			queueName := consumer.GetQueueName(currentModulo)
 			consumerGroupName := consumer.GetGroupName(currentModulo, groupNameSuffix)
 
