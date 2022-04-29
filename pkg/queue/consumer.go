03b7ac3d98f9026af665bb681dea5c7caef011bd
Iliyan <iliyan.motovski@coretrix.com>
2022-04-29 13:40:45 +0300

    enable consumers scale

--- pkg/queue/consumer.go
+++ pkg/queue/consumer.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"strings"
 	"sync"
@@ -237,4 +238,181 @@ func (r *ConsumerRunner) RunConsumerManyByModulo(consumer ConsumerManyByModulo, 
 
 	outerWG.Wait()
 	log.Printf("RunConsumerManyByModulo exited (%s)", baseQueueName)
+}
+
+type ScalableConsumerRunner struct {
+	ctx       context.Context
+	redisPool string
+}
+
+func NewScalableConsumerRunner(ctx context.Context, redisPool string) *ScalableConsumerRunner {
+	return &ScalableConsumerRunner{ctx: ctx, redisPool: redisPool}
+}
+
+func (r *ScalableConsumerRunner) RunScalableConsumerMany(consumer ConsumerMany, groupNameSuffix *string, prefetchCount int) {
+	ormService := service.DI().OrmEngine().Clone()
+	redis := ormService.GetRedis(r.redisPool)
+
+	queueName := consumer.GetQueueName()
+	consumerGroupName := consumer.GetGroupName(groupNameSuffix)
+
+	currentIndex := addConsumerGroup(redis, consumerGroupName)
+
+	log.Printf("RunScalableConsumerMany index (%d) initialized (%s)", currentIndex, queueName)
+
+	eventsConsumer := ormService.GetEventBroker().Consumer(consumerGroupName)
+
+	for {
+		// eventsConsumer.ConsumeMany should block and not return anything
+		// if it returns true => this consumer is exited with no errors, but still not consuming
+		// if it returns false => this consumer is exited with error "could not obtain lock", so we should retry
+		if exitedWithNoErrors := eventsConsumer.ConsumeMany(r.ctx, currentIndex, prefetchCount, func(events []beeorm.Event) {
+			log.Printf("We have %d new dirty events in %s", len(events), queueName)
+
+			if err := consumer.Consume(ormService, events); err != nil {
+				removeConsumerGroup(eventsConsumer, redis, consumerGroupName, currentIndex)
+				panic(err)
+			}
+
+			log.Printf("We consumed %d dirty events in %s", len(events), queueName)
+		}); !exitedWithNoErrors {
+			log.Printf("RunScalableConsumerMany failed to start (%s) - retrying in %.1f seconds", queueName, obtainLockRetryDuration.Seconds())
+			time.Sleep(obtainLockRetryDuration)
+			continue
+		} else {
+			log.Println("eventsConsumer.ConsumeMany returned true")
+			break
+		}
+	}
+
+	removeConsumerGroup(eventsConsumer, redis, consumerGroupName, currentIndex)
+
+	log.Printf("RunScalableConsumerMany exited (%s)", queueName)
+}
+
+func (r *ScalableConsumerRunner) RunScalableConsumerOne(consumer ConsumerOne, groupNameSuffix *string, prefetchCount int) {
+	ormService := service.DI().OrmEngine().Clone()
+	redis := ormService.GetRedis(r.redisPool)
+
+	queueName := consumer.GetQueueName()
+	consumerGroupName := consumer.GetGroupName(groupNameSuffix)
+
+	currentIndex := addConsumerGroup(redis, consumerGroupName)
+
+	log.Printf("RunScalableConsumerOne index (%d) initialized (%s)", currentIndex, queueName)
+
+	eventsConsumer := ormService.GetEventBroker().Consumer(consumerGroupName)
+
+	for {
+		// eventsConsumer.ConsumeMany should block and not return anything
+		// if it returns true => this consumer is exited with no errors, but still not consuming
+		// if it returns false => this consumer is exited with error "could not obtain lock", so we should retry
+		if exitedWithNoErrors := eventsConsumer.ConsumeMany(r.ctx, currentIndex, prefetchCount, func(events []beeorm.Event) {
+			log.Printf("We have %d new dirty events in %s", len(events), queueName)
+
+			for _, event := range events {
+				if err := consumer.Consume(ormService, event); err != nil {
+					removeConsumerGroup(eventsConsumer, redis, consumerGroupName, currentIndex)
+					panic(err)
+				}
+				event.Ack()
+			}
+
+			log.Printf("We consumed %d dirty events in %s", len(events), queueName)
+		}); !exitedWithNoErrors {
+			log.Printf("RunScalableConsumerOne failed to start (%s) - retrying in %.1f seconds", queueName, obtainLockRetryDuration.Seconds())
+			time.Sleep(obtainLockRetryDuration)
+			continue
+		} else {
+			log.Println("eventsConsumer.ConsumeMany returned true")
+			break
+		}
+	}
+
+	removeConsumerGroup(eventsConsumer, redis, consumerGroupName, currentIndex)
+
+	log.Printf("RunScalableConsumerOne exited (%s)", queueName)
+}
+
+const consumerGroupsKey = "consumer_groups"
+
+type indexer struct {
+	LatestIndex           int
+	ActiveConsumerIndexes map[int]*struct{}
+}
+
+func addConsumerGroup(redis *beeorm.RedisCache, consumerGroupName string) int {
+	indexerValue, err := getConsumerGroupIndexer(redis, consumerGroupName)
+	if err != nil {
+		panic(err)
+	}
+
+	if indexerValue == nil {
+		indexerValue = &indexer{}
+	}
+	if indexerValue.ActiveConsumerIndexes == nil {
+		indexerValue.ActiveConsumerIndexes = map[int]*struct{}{}
+	}
+
+	indexerValue.LatestIndex++
+	indexerValue.ActiveConsumerIndexes[indexerValue.LatestIndex] = &struct{}{}
+
+	err = setConsumerGroupIndexer(redis, consumerGroupName, indexerValue)
+	if err != nil {
+		panic(err)
+	}
+
+	return indexerValue.LatestIndex
+}
+
+func removeConsumerGroup(consumer beeorm.EventsConsumer, redis *beeorm.RedisCache, consumerGroupName string, indexToRemove int) {
+	indexerValue, err := getConsumerGroupIndexer(redis, consumerGroupName)
+	if err != nil {
+		panic(err)
+	}
+
+	delete(indexerValue.ActiveConsumerIndexes, indexToRemove)
+
+	err = setConsumerGroupIndexer(redis, consumerGroupName, indexerValue)
+	if err != nil {
+		panic(err)
+	}
+
+	// transfer pending items from stopped consumer to another if available as per: https://beeorm.io/guide/event_broker.html#consumers-scaling
+	if len(indexerValue.ActiveConsumerIndexes) != 0 {
+		indexToTransferClaimedItems := 0
+		for index := range indexerValue.ActiveConsumerIndexes {
+			indexToTransferClaimedItems = index
+			break
+		}
+
+		if indexToTransferClaimedItems != 0 {
+			log.Printf("claiming from %d to %d", indexToRemove, indexToTransferClaimedItems)
+			consumer.Claim(indexToRemove, indexToTransferClaimedItems)
+		}
+	}
+}
+
+func setConsumerGroupIndexer(redis *beeorm.RedisCache, consumerGroupName string, indexer *indexer) error {
+	marshaled, err := json.Marshal(indexer)
+	if err != nil {
+		return err
+	}
+
+	redis.HSet(consumerGroupsKey, consumerGroupName, marshaled)
+	return err
+}
+
+func getConsumerGroupIndexer(redis *beeorm.RedisCache, consumerGroupName string) (*indexer, error) {
+	marshaled, has := redis.HGet(consumerGroupsKey, consumerGroupName)
+	if !has {
+		return nil, nil
+	}
+
+	indexer := &indexer{}
+	if err := json.Unmarshal([]byte(marshaled), indexer); err != nil {
+		return nil, err
+	}
+
+	return indexer, nil
 }
