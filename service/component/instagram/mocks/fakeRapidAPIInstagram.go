fb3bc9bac6d62dc1d447f90e28e29d65cb8cf97f
Krasimir Ivanov <krasimir.ivanov@coretrix.com>
2022-05-18 10:14:47 +0300

    instagram fake fixed

--- service/component/instagram/mocks/fakeRapidAPIInstagram.go
+++ service/component/instagram/mocks/fakeRapidAPIInstagram.go
@@ -6,20 +6,20 @@ import (
 	"github.com/coretrix/hitrix/service/component/instagram"
 )
 
-type FakeRapidAPIInstagram85 struct {
+type FakeRapidAPIInstagram struct {
 	mock.Mock
 }
 
-func (f *FakeRapidAPIInstagram85) GetName() string {
+func (f *FakeRapidAPIInstagram) GetName() string {
 	return f.Called().Get(0).(string)
 }
 
-func (f *FakeRapidAPIInstagram85) GetAccount(_ string) (*instagram.Account, error) {
+func (f *FakeRapidAPIInstagram) GetAccount(_ string) (*instagram.Account, error) {
 	args := f.Called()
 	return args.Get(0).(*instagram.Account), args.Error(1)
 }
 
-func (f *FakeRapidAPIInstagram85) GetFeed(_ int64, _ string) ([]*instagram.Post, string, error) {
+func (f *FakeRapidAPIInstagram) GetFeed(_ int64, _ string) ([]*instagram.Post, string, error) {
 	args := f.Called()
 	return args.Get(0).([]*instagram.Post), args.String(1), args.Error(2)
 }
