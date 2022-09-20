88fc46927542e943fecd6a3d8e9b693b118c9197
Masih Yeganeh <goodboy.php@gmail.com>
2022-09-20 18:29:00 +0430

    Fix instagram bug

--- service/component/instagram/rapidAPIInstagram85.go
+++ service/component/instagram/rapidAPIInstagram85.go
@@ -223,6 +223,10 @@ func sendRapidRequest(provider rapidAPIProvider, url string) ([]byte, error) {
 	}
 	defer res.Body.Close()
 
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Status code %d: %s", res.StatusCode, res.Status)
+	}
+
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
