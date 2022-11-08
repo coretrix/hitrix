cb8dc0b04839cbd4c41c25dfdeae6619beb6d958
Anton <a.shumansky@gmail.com>
2022-11-08 22:25:54 +0200

    add tests for timestamp_gte_now

--- test/validator_test.go
+++ test/validator_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -21,5 +22,15 @@ func TestCountryCodeValidation(t *testing.T) {
 	assert.NotNil(t, err)
 
 	err = helper.NewValidator().Validate("SE", "country_code_custom")
+	assert.Nil(t, err)
+}
+
+func TestTimestampGteNowValidation(t *testing.T) {
+	valueAfterNow := time.Now().Add(time.Duration(12312312312))
+	valueNow := time.Now()
+	err := helper.NewValidator().Validate(helper.GetTimestamp(&valueNow), "timestamp_gte_now")
+	assert.NotNil(t, err)
+
+	err = helper.NewValidator().Validate(helper.GetTimestamp(&valueAfterNow), "timestamp_gte_now")
 	assert.Nil(t, err)
 }
