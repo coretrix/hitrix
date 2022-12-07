7a35d28ab4bac56ee733deaaeae71daaf8415c65
h-khodadadeh <khodadadeh@coretrix.com>
2022-12-07 15:20:28 +0330

    removed feature_flag_with_cache.go

--- service/registry/feature_flag.go
+++ service/registry/feature_flag.go
@@ -7,7 +7,6 @@ import (
 	"github.com/sarulabs/di"
 
 	"github.com/coretrix/hitrix/service"
-	"github.com/coretrix/hitrix/service/component/clock"
 	errorlogger "github.com/coretrix/hitrix/service/component/error_logger"
 	featureflag "github.com/coretrix/hitrix/service/component/feature_flag"
 )
@@ -26,26 +25,6 @@ func ServiceProviderFeatureFlag(registry FeatureFlagRegistryInitFunc) *service.D
 
 			errorLoggerService := ctn.Get(service.ErrorLoggerService).(errorlogger.ErrorLogger)
 			featureFlagService := featureflag.NewFeatureFlagService(errorLoggerService)
-			registry(featureFlagService)
-
-			return featureFlagService, nil
-		},
-	}
-}
-func ServiceProviderFeatureFlagWithCache(registry FeatureFlagRegistryInitFunc) *service.DefinitionGlobal {
-	return &service.DefinitionGlobal{
-		Name: service.FeatureFlagService,
-		Build: func(ctn di.Container) (interface{}, error) {
-			ormConfig := ctn.Get(service.ORMConfigService).(beeorm.ValidatedRegistry)
-			entities := ormConfig.GetEntities()
-			if _, ok := entities["entity.FeatureFlagEntity"]; !ok {
-				return nil, errors.New("you should register FeatureFlagEntity")
-			}
-
-			errorLoggerService := ctn.Get(service.ErrorLoggerService).(errorlogger.ErrorLogger)
-			clockService := ctn.Get(service.ClockService).(clock.IClock)
-
-			featureFlagService := featureflag.NewFeatureFlagWithCacheService(errorLoggerService, clockService)
 			registry(featureFlagService)
 
 			return featureFlagService, nil
--- test/feature_flag_test.go
+++ test/feature_flag_test.go
@@ -60,29 +60,3 @@ func TestFeatureFlag(t *testing.T) {
 	assert.True(t, featureFlagService.IsActive(ormService, productCollectionFeature))
 	assert.Nil(t, featureFlagService.FailIfIsNotActive(ormService, productCollectionFeature))
 }
-func TestFeatureFlagWithCache(t *testing.T) {
-	createContextMyApp(t, "my-app", nil,
-		[]*service.DefinitionGlobal{
-			registry.ServiceProviderErrorLogger(),
-			registry.ServiceProviderClock(),
-			registry.ServiceProviderFeatureFlagWithCache(initFeatures),
-		},
-		nil,
-	)
-
-	featureFlagService := service.DI().FeatureFlag()
-	featureFlagService.Register(&ProductCollection{})
-
-	ormService := service.DI().OrmEngine()
-	clockService := service.DI().Clock()
-
-	featureFlagService.Sync(ormService, clockService)
-
-	assert.False(t, featureFlagService.IsActive(ormService, productCollectionFeature))
-	assert.Error(t, featureFlagService.FailIfIsNotActive(ormService, productCollectionFeature))
-
-	assert.Nil(t, featureFlagService.Enable(ormService, productCollectionFeature))
-
-	assert.True(t, featureFlagService.IsActive(ormService, productCollectionFeature))
-	assert.Nil(t, featureFlagService.FailIfIsNotActive(ormService, productCollectionFeature))
-}
