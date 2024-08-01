013b65bda2a786c43f97da788c6100dd3026022e
Krasimir Ivanov <krasimir.ivanov@coretrix.com>
2024-08-02 00:41:37 +0300

    fix example

--- example/server.go
+++ example/server.go
@@ -41,7 +41,7 @@ func main() {
 		registry.ServiceProviderOTP(nil),
 	).RegisterDIRequestService(
 		registry.ServiceProviderOrmEngineForContext(false),
-	).RegisterRedisPools(&app.RedisPools{Persistent: "default", Cache: "default", Search: "search_pool", Stream: "stream_pool"}).
+	).RegisterRedisPools(&app.RedisPools{Persistent: "default", Cache: "default", Search: []string{"search_pool", "search_pool2"}, Stream: "stream_pool"}).
 		RegisterDevPanel(&entity.DevPanelUserEntity{}, middleware.DevPanelRouter).Build()
 	defer deferFunc()
 
